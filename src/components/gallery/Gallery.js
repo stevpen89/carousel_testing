e34318a31428d8100128db1840053b8c250e7c94
Steven Penfield <stevenpenfield9@gmail.com>
2019-10-14 15:28:18 -0600

    adjusted the carousel with a tags to allow image linking based on displaying item

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -25,33 +25,33 @@ const slideshow = () => {
     <div className="slide-container">
       <Fade {...fadeProperties}>
         <div className="each-fade">
-          <div className="image-container">
-            <img src={fadeImages[0]} />
-          </div>
+          <a href="https://www.google.com" className="image-container">
+            <img src={fadeImages[0]} alt="" />
+          </a>
           <h2>First Slide</h2>
         </div>
         <div className="each-fade">
-          <div className="image-container">
-            <img src={fadeImages[1]} />
-          </div>
+          <a href="https://www.google.com" className="image-container">
+            <img src={fadeImages[1]} alt="" />
+          </a>
           <h2>Second Slide</h2>
         </div>
         <div className="each-fade">
-          <div className="image-container">
-            <img src={fadeImages[2]} />
-          </div>
+          <a href="https://www.google.com" className="image-container">
+            <img src={fadeImages[2]} alt="" />
+          </a>
           <h2>Third Slide</h2>
         </div>
         <div className="each-fade">
-          <div className="image-container">
-            <img src={fadeImages[3]} />
-          </div>
+          <a href="https://www.google.com" className="image-container">
+            <img src={fadeImages[3]} alt="" />
+          </a>
           <h2>Fourth Slide</h2>
         </div>
         <div className="each-fade">
-          <div className="image-container">
-            <img src={fadeImages[4]} />
-          </div>
+          <a href="https://www.google.com" className="image-container">
+            <img src={fadeImages[4]} alt="" />
+          </a>
           <h2>Fifth Slide</h2>
         </div>
       </Fade>
